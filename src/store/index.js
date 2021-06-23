75d835a52f4b1966ccc1d673f7d0fd2a28c4442c
OliverRedman <oliredman@hotmail.com>
2021-06-23 17:11:29 +0100

    start

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,31 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
-
+Vue.use(Vuex);
+// dSV_LkG0DCu8nF2cp2O-ldD-_wfxdQ
 export default new Vuex.Store({
   state: {
+    posts: [],
   },
-  mutations: {
-  },
+  mutations: {},
   actions: {
+    async getPosts(context) {
+      const url = `https://www.reddit.com/r/memes/top.json`;
+      const response = await fetch(url, {
+        headers: {
+          accept: "application/json",
+        },
+      });
+      const data = await response.json();
+      if (response.ok) {
+        context.state.posts = data.data.children;
+        console.log(data.data.children);
+        return;
+      }
+      const error = new Error(data.message || "Failed to load posts");
+      error.response = data;
+      throw error;
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div v-for="post of posts" class="post" :key="post.id">
+      {{ post.data.title }} <img :src="post.data.thumbnail" alt="" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  created() {
+    this.$store.dispatch("getPosts");
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts;
+    },
+  },
+};
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.post {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.post img {
+  width: 100px;
+}
+</style>
